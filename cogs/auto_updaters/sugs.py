15b3b7c9b038501dd387c8eda8d630f86a7ae8aa
Irony00 <r.rahul.developer@gmail.com>
2022-03-20 22:34:06 +0530

    configured easters

--- cogs/auto_updaters/sugs.py
+++ cogs/auto_updaters/sugs.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 import random
 
-outside_server_easters = ["You can get support here:- https://discord.gg/UYBKcTYzyU"]
-easter = ["The credit for the spicy hot waifu and husbando pics go to Tanjiro#3129, yami#2835, SALMON#8537. True legends :pray: ","Keep the rules in mind and enjoy your time here ^-^","You can give your feedback abt the bot using the feedback command"]
+outside_server_easters = ["You can get support here:- https://discord.gg/74cG7UW65Y"]
+easter = ["Keep the rules in mind and enjoy your time here ^-^",
+          "You can give your feedback abt the bot using the feedback command"]
 
 class SugsCog(commands.Cog):
     def __init__(self, bot):
@@ -15,7 +16,7 @@ class SugsCog(commands.Cog):
       if adPer < 2:
         easter_choice = random.choice(easter + outside_server_easters)
         
-        insomnia_server_id = "898127603309899806"
+        insomnia_server_id = "952638468763316384"
         if ctx.guild.id == insomnia_server_id and easter_choice in outside_server_easters:
             return
 
